d0afd88bdb5f3d0351bcce23a122f6f27abf38cc
Nick Bilsborrow <nickbilsborrow15@gmail.com>
2021-08-06 16:07:39 +0100

    extra functionality added to match front end

--- src/users/users.controllers.js
+++ src/users/users.controllers.js
@@ -25,8 +25,11 @@ exports.findUser = async (req, res) => {
 exports.removeUser = async (req, res) => {
   try {
     const user = req.body.username;
-    const password = req.body.password
-    const removedUser = await User.findOneAndDelete({ username: user, password: password });
+    const password = req.body.password;
+    const removedUser = await User.findOneAndDelete({
+      username: user,
+      password: password,
+    });
     res.status(200).send({ user: removedUser, message: "User removed" });
   } catch (error) {
     res.status(500).send(error);
@@ -34,27 +37,24 @@ exports.removeUser = async (req, res) => {
 };
 
 exports.updateUser = async (req, res) => {
-  
   try {
-    console.log(req.body.oldPassword)
+    console.log(req.body.oldPassword);
     let updateUser;
-console.log(6)
     const filter = req.body.oldUsername;
     const oldPass = req.body.oldPassword;
     const user = req.body.newUsername;
     const newPass = req.body.newPassword;
     const email = req.body.email;
-console.log(req.body.newPassword)
+    console.log(req.body.newPassword);
     if (user) {
       updateUser = await User.updateOne(
         { username: filter },
         { username: user }
       );
     } else if (newPass) {
-      console.log(1)
-      console.log({ username: filter, password: oldPass })
+      console.log({ username: filter, password: oldPass });
       updateUser = await User.updateOne(
-        { username: filter, password: oldPass},
+        { username: filter, password: oldPass },
         { password: newPass }
       );
     } else if (email) {
@@ -65,6 +65,94 @@ console.log(req.body.newPassword)
     }
 
     res.status(200).send({ user: updateUser, message: "User modified" });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+exports.getUserWatched = async (req, res) => {
+  try {
+    const targetUser =await User.findOne({ username: req.params.username });
+    
+    res.status(200).send(targetUser.watched);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+exports.addUserWatched = async (req, res) => {
+  try {
+
+    console.log(req.body.username)
+    console.log(req.body.movie)
+    const user = await User.findOne({ username: req.body.username });
+    
+    const updateUser = await User.updateOne(
+      { username: req.body.username },
+      {$push: {watched: [req.body.movie]}}
+    );
+    console.log(updateUser)
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+exports.removeUserWatched = async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    const index = user.watched.indexOf(req.body.movie)
+    user.watched.splice(index,1)    
+    const updateUser = await User.findOneAndUpdate(
+      { username: req.body.username },
+      user
+    );
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+exports.getUserWatchlist = async (req, res) => {
+  try {
+    console.log(req.params.username)
+    const targetUser = await User.findOne({ username: req.params.username });
+    console.log(targetUser)
+    res.status(200).send(targetUser.watchlist);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+exports.addUserWatchlist = async (req, res) => {
+  try {
+   
+    const user = await User.findOne({ username: req.body.username });
+    
+    user.watchlist.push(req.body.movie);
+    
+    const updateUser =await User.findOneAndUpdate(
+      { username: req.body.username },
+      user
+    );
+    res.status(200).send({newWatchlist:req.body.movie,message:"new movie added to watch list"})
+  } catch (error) {
+    res.status(500).send(error);
+    console.log(error)
+  }
+};
+
+exports.removeUserWatchlist = async (req, res) => {
+  try {
+    console.log(req.body.username)
+    console.log(req.body.movie)
+    const user = await User.findOne({ username: req.body.username });
+    console.log(user)
+    const index = user.watchlist.indexOf(req.body.movie)
+    console.log(index)
+    user.watchlist.splice(index,1)    
+    const updateUser = await User.findOneAndUpdate(
+      { username: req.body.username },
+      user
+    );
   } catch (error) {
     res.status(500).send(error);
   }
--- src/users/users.routes.js
+++ src/users/users.routes.js
@@ -1,11 +1,16 @@
 const {Router} = require('express');
 const userRouter = Router();
-const {createUser, findUser, updateUser, removeUser} = require('./users.controllers')
+const {createUser, findUser, updateUser, removeUser, getUserMovies, getUserWatched, addUserWatched, removeUserWatched, getUserWatchlist, addUserWatchlist, removeUserWatchlist} = require('./users.controllers')
 
 userRouter.post('/users',createUser);
 userRouter.get('/users/:username',findUser);
 userRouter.put('/users',updateUser);
 userRouter.delete('/users/:username',removeUser);
 userRouter.post('/deleteUsers' ,removeUser)
-
+userRouter.get('/users/watched/:username',getUserWatched)
+userRouter.post('/users/watched/add',addUserWatched)
+userRouter.post('/users/watched/remove',removeUserWatched)
+userRouter.get('/users/watchlist/:username',getUserWatchlist)
+userRouter.post('/users/watchlist/add',addUserWatchlist)
+userRouter.post('/users/watchlist/remove',removeUserWatchlist)
 module.exports = userRouter
